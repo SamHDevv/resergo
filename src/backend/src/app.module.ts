6b09f7c6e56e86c5340ad8a092741cec97aed8e6
Samuel Hernández <samuel.hernandez.ext@rsi.cajarural.com>
2024-09-26 20:31:17 +0200

    updating

--- src/backend/src/app.module.ts
+++ src/backend/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { DatabaseModule } from './database/database.module';
+import { DataLayerModule } from './app/data-layer/data-layer.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DataLayerModule],
   controllers: [AppController],
   providers: [
     AppService
